36a7c5c399ccfc286a7ee034532736fdc297401f
tomas <tomas@TomasGudi>
2024-11-04 17:48:37 -0300

    inicializacion

---
+++ src/main/java/ar/edu/unju/escmi/entities/Cliente.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.escmi.entities;
+
+
+public class Cliente {
+
+
+}
---
+++ src/main/java/ar/edu/unju/escmi/main/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.escmi.main;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		
+	}
+
+}
