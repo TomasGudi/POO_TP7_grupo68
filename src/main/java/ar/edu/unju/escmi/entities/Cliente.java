216ffdffc892536fce454a3886b88e06b5ffe566
Usuario <Usuario@DESKTOP-GFBSO7Q>
2024-11-05 17:20:32 -0300

    clase factura y cliente , con su implementacion DAO Juan garzon

--- src/main/java/ar/edu/unju/escmi/entities/Cliente.java
+++ src/main/java/ar/edu/unju/escmi/entities/Cliente.java
@@ -1,7 +1,89 @@
 package ar.edu.unju.escmi.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "nombre", nullable = false, length = 50)
+	private String nombre;
+	
+	
+	@Column(name = "apellido", nullable = false, length = 50)
+    private String apellido;
 
+    @Column(name = "domicilio", length = 100)
+    private String domicilio;
 
+    @Column(name = "dni", nullable = false, unique = true) 
+    private int dni;
+
+    @Column(name = "estado") 
+    private boolean estado;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDomicilio() {
+		return domicilio;
+	}
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	public Cliente(int id, String nombre, String apellido, String domicilio, int dni, boolean estado) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.domicilio = domicilio;
+		this.dni = dni;
+		this.estado = estado;
+	}
+	public Cliente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", domicilio=" + domicilio
+				+ ", dni=" + dni + ", estado=" + estado + "]";
+	}
+	
+	
 }
